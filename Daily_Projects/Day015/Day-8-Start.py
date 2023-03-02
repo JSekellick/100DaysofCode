46d7da4746fec32fd8ec99861f87865f24367686
Jared_S <67425012+JSekellick@users.noreply.github.com>
2023-03-01 23:09:01 -0500

    Day15 and started paint are calculator

---
+++ Daily_Projects/Day015/Day-8-Start.py
@@ -0,0 +1,28 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#     print("Hello Handsome")
+#     print("Why do you smell like a sad clown?")
+#     print("Frost is not frozen dew!")
+
+# greet()
+
+# Function that allows for input
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"Why do you smell so bad, {name}?")
+
+# greet_with_name("Carl")
+
+
+# Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello, {name}.")
+    print(f"How is, {location}?")
+
+
+greet_with("Bill", "Shunk")
