58f6d1f34faf9702ca749198cf4b26da6b98ae1d
Jared Sekellick <67425012+Barbuckles@users.noreply.github.com>
2023-03-11 21:38:29 -0500

    Day022 and some extra list practice

---
+++ Daily_Projects/Days_21-40/Day023/Day010-start-MyCode.py
@@ -0,0 +1,21 @@
+# Functions with inputs
+#def my_function(something):
+    # Do this with somthing
+    # Then do this
+    # Finally do this
+
+# def my_function():
+#     result = 3 * 2
+#     return result
+
+# output = my_function()
+
+# Functions with Outputs
+
+def format_name(f_name, l_name):
+    f_name = input("What is your first name? ")
+    l_name = input("What is your last name? ")
+    formated_f_name = f_name.title
+    formated_l_name = l_name.title  
+
+format_name("Gary", "Blueberry")
