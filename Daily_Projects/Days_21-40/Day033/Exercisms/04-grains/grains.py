a1453552153a0026a48e23ea3ad78aa3de8a44ff
Jared Sekellick <67425012+Barbuckles@users.noreply.github.com>
2023-03-25 21:58:17 -0400

    Day 33

---
+++ Daily_Projects/Days_21-40/Day033/Exercisms/04-grains/grains.py
@@ -0,0 +1,10 @@
+def square(number):
+    grains = number * number
+    return grains
+# when the square value is not in the acceptable range        
+# raise ValueError("square must be between 1 and 64")
+
+def total():
+    for grain in range(1, 65):
+        grains += square(number)
+        return grains
