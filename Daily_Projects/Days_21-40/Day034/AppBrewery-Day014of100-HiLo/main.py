0d6e517c1890851bba0e28d4effe24156b61e39e
Jared Sekellick <67425012+Barbuckles@users.noreply.github.com>
2023-03-29 12:46:18 -0400

    Day034 added

---
+++ Daily_Projects/Days_21-40/Day034/AppBrewery-Day014of100-HiLo/main.py
@@ -0,0 +1,39 @@
+# Hi Lo Game
+# Jared Sekellick
+
+import random, os
+
+from art import logo, vs
+from game_data import data
+
+# game start and into
+def main_screen():
+    print("Welcome to")
+    print(logo)
+
+# Generate a random account from the game data.
+def random_account():
+
+
+# Format account data into printable format.
+
+
+#return f"Compare {A or B} {name}, {description}, from {country}"
+    
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count.
+## If Statement
+
+# Feedback.
+
+# Score Keeping.
+
+# Make game repeatable.
+
+# Make B become the next A.
+
+# Add art.
+
+# Clear screen between rounds.
