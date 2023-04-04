002be80f520e7d9fdf94da79a62dbc35ff979217
Jared_S <67425012+JSekellick@users.noreply.github.com>
2023-04-03 20:58:57 -0400

    Added day 36

---
+++ Daily_Projects/Days_21-40/Day035/Dictionary.py
@@ -0,0 +1,7 @@
+# Create a dictionary with the following key-value pairs:
+# "name" - "John", "age" - 25, "city" - "New York"
+my_dict = {
+    "name" : "John",
+    "age" : 25,
+    "city" : "New York",
+}
